bd07b156033b8464b954fb88cef14861f4a4a525
Luis Fernando <fernando.osousa@alu.ufc.br>
2021-11-26 10:25:43 -0300

    add rotas

--- src/index.js
+++ src/index.js
@@ -6,17 +6,19 @@ import Home from './pages/Home/index'
 import CuratorItem from "./components/Curator"
 import Footer from "./components/Footer"
 import Places from './components/Places';
+import Routes from './router';
 
 const CONFIG = {
   city: "Lagoa do Carneiro",
   state: "CE"
 }
 
+
 ReactDOM.render(
   <div className="main__wrapper">
     <div className="main__container">
       <Header city={CONFIG.city} state={CONFIG.state} />
-      <Home city={CONFIG.city} />
+      <Routes city={CONFIG.city} state={CONFIG.state} />
       <Places/>
       <CuratorItem />
     </div>
